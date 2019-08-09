81f532d7ed2ac480c0cab28c271f9369c24a9a04
Felix Palta <felix@example.com>
2019-08-09 20:26:15 +0300

    Move allocator to lib.hpp

--- lib.hpp
+++ lib.hpp
@@ -1,7 +1,67 @@
 #pragma once
+#include <vector>
+#include <cstddef>
 
 namespace otus {
 
 int fact(int i);
+
+template <typename T>
+struct ReservingAllocator {
+    using value_type = T;
+    using pointer = value_type*;
+
+    ReservingAllocator(size_t poolSize) noexcept
+        : mPoolSize(poolSize)
+    {
+    }
+
+    ReservingAllocator(const ReservingAllocator & other)
+        : mPoolSize(other.mPoolSize)
+    {
+        if (other.mPool.capacity()) {
+            mPool.reserve(other.mPool.capacity());
+            for (const auto & a : other.mPool)
+                mPool.emplace_back(a);
+        }
+    }
+
+    size_t pool_size() const { return mPoolSize; }
+
+    template <class U> ReservingAllocator(ReservingAllocator<U> const& other) noexcept
+        : mPoolSize(other.pool_size())
+    {
+    }
+
+    pointer allocate(std::size_t n) {
+        size_t oldSize = mPool.size();
+        size_t newSize = mPool.size() + n;
+        if (newSize > mPoolSize)
+            throw std::bad_alloc();
+        init_pool();
+        mPool.resize(newSize);
+        return &mPool[oldSize];
+    }
+
+    void deallocate(pointer /*p*/, std::size_t /*n*/) noexcept {
+
+    }
+
+private:
+    void init_pool() {
+        if (!mPool.capacity()) {
+            mPool.reserve(mPoolSize);
+        }
+    }
+private:
+    const size_t mPoolSize;
+    std::vector<T> mPool;
+};
+
+template <typename T, typename U>
+bool operator==(ReservingAllocator<T> const & a, ReservingAllocator<U> const & b) noexcept
+{
+    return &a == &b;
+}
 
 } // otus
--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include "lib.hpp"
 #include <iostream>
-#include <vector>
 #include <map>
-#include <memory>
 
 template <typename MapType, typename ValueFunc>
 void populate_map(MapType & map, int startKey, int numKeys, ValueFunc func)
@@ -19,64 +17,6 @@ void print_map(const MapType & map) {
         std::cout << p.first << " " << p.second << std::endl;
 }
 
-template <typename T>
-struct ReservingAllocator {
-    using value_type = T;
-    using pointer = value_type*;
-
-    ReservingAllocator(size_t poolSize) noexcept
-        : mPoolSize(poolSize)
-    {
-    }
-
-    ReservingAllocator(const ReservingAllocator & other)
-        : mPoolSize(other.mPoolSize)
-    {
-        if (other.mPool.capacity()) {
-            mPool.reserve(other.mPool.capacity());
-            for (const auto & a : other.mPool)
-                mPool.emplace_back(a);
-        }
-    }
-
-    size_t pool_size() const { return mPoolSize; }
-
-    template <class U> ReservingAllocator(ReservingAllocator<U> const& other) noexcept
-        : mPoolSize(other.pool_size())
-    {
-    }
-    
-    pointer allocate(std::size_t n) {
-        size_t oldSize = mPool.size();
-        size_t newSize = mPool.size() + n;
-        if (newSize > mPoolSize)
-            throw std::bad_alloc();
-        init_pool();
-        mPool.resize(newSize);
-        return &mPool[oldSize];
-    }
-
-    void deallocate(pointer /*p*/, std::size_t /*n*/) noexcept {
-
-    }
-
-private:
-    void init_pool() {
-        if (!mPool.capacity()) {
-            mPool.reserve(mPoolSize);
-        }
-    }
-private:
-    const size_t mPoolSize;
-    std::vector<T> mPool;
-};
-
-template <typename T, typename U>
-bool operator==(ReservingAllocator<T> const & a, ReservingAllocator<U> const & b) noexcept
-{
-    return &a == &b;
-}
-
 int main() {
 
     {
@@ -85,7 +25,7 @@ int main() {
     }
 
     {
-        std::map<int, int, std::less<int>, ReservingAllocator<int>> map_custom_alloc(ReservingAllocator<int>(10));
+        std::map<int, int, std::less<int>, otus::ReservingAllocator<int>> map_custom_alloc(otus::ReservingAllocator<int>(10));
         populate_map(map_custom_alloc, 0, 10, otus::fact);
         print_map(map_custom_alloc);
     }
