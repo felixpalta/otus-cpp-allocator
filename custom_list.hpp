55de1dfea794aebfae977a499ed130ea14419ce1
Felix Palta (office) <fpalta90@gmail.com>
2019-09-06 19:52:53 +0300

    Revert "Remove all before review"
    This reverts commit 4f03cb11e8aa2745375065b3f34d54077f3c6ab5.

---
+++ custom_list.hpp
@@ -0,0 +1,123 @@
+#ifndef CUSTOM_LIST_HPP
+#define CUSTOM_LIST_HPP
+
+#include <memory>
+
+namespace otus {
+
+template <typename T, typename A = std::allocator<T>>
+class CustomList
+{
+public:
+    using value_type = T;
+    using allocator_type = A;
+    explicit CustomList(const allocator_type & a = allocator_type())
+        : mAllocator(a)
+        , mRootNode(nullptr)
+    {}
+
+    ~CustomList();
+
+    struct iterator
+    {
+        using value_type = CustomList::value_type;
+        using iterator_category = std::forward_iterator_tag;
+        using difference_type = size_t;
+        using pointer = value_type*;
+        using reference = value_type&;
+
+        void * node;
+        iterator() noexcept : node(nullptr) {}
+        explicit iterator(void * n) : node(n) {}
+        iterator & operator++();
+        reference operator*();
+        bool operator==(const iterator & other);
+        bool operator!=(const iterator & other);
+    };
+
+    iterator push_back(const value_type & val);
+    iterator begin();
+    iterator end();
+
+    bool empty() const { return mRootNode == nullptr; }
+
+private:
+    struct Node {
+        value_type value;
+        Node * next;
+        explicit Node(const value_type & val) : value(val), next(nullptr) {}
+        Node() : value(), next(nullptr) {}
+    };
+
+    using allocator_type_internal = typename allocator_type::template rebind<Node>::other;
+    allocator_type_internal mAllocator;
+    Node * mRootNode;
+};
+
+template<typename T, typename A>
+typename CustomList<T, A>::iterator CustomList<T, A>::begin()
+{
+    return iterator{static_cast<void*>(mRootNode)};
+}
+
+template<typename T, typename A>
+typename CustomList<T, A>::iterator CustomList<T, A>::end()
+{
+    return iterator{nullptr};
+}
+
+template<typename T, typename A>
+typename CustomList<T, A>::iterator CustomList<T, A>::push_back(const value_type & val)
+{
+    Node ** targetNodePtr = &mRootNode;
+    while (*targetNodePtr) {
+        targetNodePtr = &((*targetNodePtr)->next);
+    }
+    *targetNodePtr = std::allocator_traits<allocator_type_internal>::allocate(mAllocator, 1);
+    std::allocator_traits<allocator_type_internal>::construct(mAllocator, *targetNodePtr, val);
+    return iterator{static_cast<void*>(*targetNodePtr)};
+}
+
+template<typename T, typename A>
+typename CustomList<T, A>::iterator::reference CustomList<T, A>::iterator::operator*()
+{
+    return static_cast<Node*>(node)->value;
+}
+
+template<typename T, typename A>
+typename CustomList<T, A>::iterator & CustomList<T, A>::iterator::operator++()
+{
+    Node * next = static_cast<Node*>(node)->next;
+    node = static_cast<void*>(next);
+    return *this;
+}
+
+template<typename T, typename A>
+bool CustomList<T, A>::iterator::operator==(const iterator &other)
+{
+    return node == other.node;
+}
+
+template<typename T, typename A>
+bool CustomList<T, A>::iterator::operator!=(const iterator &other)
+{
+    return !(*this == other);
+}
+
+template<typename T, typename A>
+CustomList<T,A>::~CustomList()
+{
+    if (!empty()) {
+        Node * next = mRootNode;
+        while (next) {
+            Node * prevNext = next;
+            next = prevNext->next;
+            std::allocator_traits<allocator_type_internal>::destroy(mAllocator, prevNext);
+            std::allocator_traits<allocator_type_internal>::deallocate(mAllocator, prevNext, 1);
+        }
+    }
+}
+
+} // otus
+
+#endif // CUSTOM_LIST_HPP
---
+++ tests_default.cpp
@@ -0,0 +1,63 @@
+#include <gtest/gtest.h>
+#include "lib.hpp"
+
+using AllocatorInt = otus::ReservingAllocator<int>;
+using AllocatorPair = otus::ReservingAllocator<std::pair<int, int>>;
+
+TEST(DefaultTests, FactTest) {
+    ASSERT_EQ(otus::fact(0), 1);
+    ASSERT_EQ(otus::fact(1), 1);
+    ASSERT_EQ(otus::fact(2), 2);
+    ASSERT_EQ(otus::fact(3), 6);
+    ASSERT_EQ(otus::fact(5), 120);
+    ASSERT_EQ(otus::fact(10), 3628800);
+}
+
+TEST(DefaultTests, AllocTest) {
+
+    AllocatorInt alloc(10);
+    ASSERT_EQ(alloc.pool_size(), 10);
+    ASSERT_EQ(alloc.current_size(), 0);
+    AllocatorInt::pointer p1 = alloc.allocate(1);
+    ASSERT_NE(p1, nullptr);
+    ASSERT_EQ(alloc.current_size(), 1);
+
+    ASSERT_EQ(alloc.allocate(1), p1+1);
+    ASSERT_EQ(alloc.current_size(), 2);
+
+    ASSERT_EQ(alloc.allocate(8), p1+2);
+    ASSERT_EQ(alloc.current_size(), 10);
+
+    ASSERT_THROW(alloc.allocate(1), std::bad_alloc);
+    ASSERT_EQ(alloc.current_size(), 10);
+}
+
+
+TEST(DefaultTests, AllocCopyTest) {
+
+    AllocatorInt alloc(10);
+    AllocatorInt alloc2(alloc);
+    ASSERT_NE(alloc, alloc2);
+    ASSERT_EQ(alloc.pool_size(), alloc2.pool_size());
+    alloc.allocate(1);
+    alloc2.allocate(10);
+    ASSERT_EQ(alloc.current_size(), 1);
+    ASSERT_EQ(alloc2.current_size(), 10);
+
+    AllocatorPair allocPair(alloc);
+    ASSERT_NE(allocPair, alloc);
+    ASSERT_EQ(allocPair.pool_size(), alloc.pool_size());
+    ASSERT_EQ(allocPair.current_size(), 0);
+}
+
+TEST(DefaultTests, CustomListTest) {
+    otus::CustomList<int> list;
+    ASSERT_TRUE(list.empty());
+    for (int i = 0; i < 5; ++i) {
+        list.push_back(i);
+    }
+    int i = 0;
+    for (auto it = list.begin(); it != list.end(); ++it, ++i) {
+        ASSERT_EQ(*it, i);
+    }
+}
